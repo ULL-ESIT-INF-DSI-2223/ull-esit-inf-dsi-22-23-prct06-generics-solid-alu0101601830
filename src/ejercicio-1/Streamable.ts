60d3f6be34c2b767c2eeefc747f9c7e1e1034f0c
Diego Arnold Wiederkehr Bruno <alu0101601830@ull.edu.es>
2023-03-07 12:50:29 +0000

    Ejercicio 1

---
+++ src/ejercicio-1/Streamable.ts
@@ -0,0 +1,18 @@
+/**
+ * A generic interface for collections of streamable items
+ * @interface Streamable
+ */
+export interface Streamable {
+    /**
+     * Búsqueda por año de un item
+     * @param {number} El año del lanzamiento del item
+     * @returns {any[]} Un array de los items que coincidan con el año de lanzamiento
+     */
+    searchItemByYear(year: number): any[];
+    /**
+     * Búsqueda por nombre de un item
+     * @param {string} El nombre del item
+     * @returns {any[]} Un array de los items que coincidan con el nombre
+     */
+    searchItemByName(name: string): any[];
+}
--- tests/basicFunctions.spec.ts
+++ tests/basicFunctions.spec.ts
@@ -1,13 +1,125 @@
-import 'mocha';
-import {expect} from 'chai';
-import {add} from "../src/basicFunctions";
+import {SeriesCollection} from "../src/ejercicio-1/SeriesCollection";
+import {DocumentalesCollection} from "../src/ejercicio-1/DocumentalesCollection";
+import {PeliculasCollection} from "../src/ejercicio-1/PeliculasCollection";
 
-describe("add function tests", () => {
-  it("add(1, 8) returns value 9", () => {
-    expect(add(1, 8)).to.be.equal(9);
+
+import { expect } from "chai";
+import "mocha";
+
+describe("Streamable", () => {
+  describe("searchItemByYear", () => {
+    it("deberia devolver un array con las series de ese año", () => {
+      const series = [
+        { nombre: "Breaking Bad", año: 2008, temporadas: 5 },
+        { nombre: "Game of Thrones", año: 2011, temporadas: 8 },
+      ];
+
+      const seriesCollection = new SeriesCollection(series);
+
+      const result = seriesCollection.searchItemByYear(2011);
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Game of Thrones",
+        año: 2011,
+        temporadas: 8,
+      });
+    });
   });
 
-  it("add(-1, 8) returns value 7", () => {
-    expect(add(-1, 8)).to.be.equal(7);
+  describe("searchItemByName", () => {
+    it("deberia devolver un array con los documentales con ese nombre", () => {
+      const documentaries = [
+        { nombre: "Planet Earth", año: 2006, duracion: 300, topico: "Nature" },
+        { nombre: "Blackfish", año: 2013, duracion: 83, topico: "Animals" },
+      ];
+
+      const documentariesCollection = new DocumentalesCollection(documentaries);
+
+      const result = documentariesCollection.searchItemByName("Blackfish");
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Blackfish",
+        año: 2013,
+        duracion: 83,
+        topico: "Animals",
+      });
+    });
+  });
+});
+
+describe("SeriesCollection", () => {
+  describe("searchItemBySeasons", () => {
+    it("deberia devolver un array de series que coincidan con el número de temporadas", () => {
+      const series = [
+        { nombre: "Breaking Bad", año: 2008, temporadas: 5 },
+        { nombre: "Game of Thrones", año: 2011, temporadas: 8 },
+        { nombre: "Friends", año: 1994, temporadas: 10 },
+      ];
+
+      const seriesCollection = new SeriesCollection(series);
+
+      const result = seriesCollection.searchItemBySeasons(8);
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Game of Thrones",
+        año: 2011,
+        temporadas: 8,
+      });
+    });
+  });
+});
+
+describe("DocumentalesCollection", () => {
+  describe("searchItemByTopic", () => {
+    it("deberia devolver un array de documentales con ese tópico", () => {
+      const documentaries = [
+        { nombre: "Planet Earth", año: 2006, duracion: 300, topico: "Nature" },
+        { nombre: "Blackfish", año: 2013, duracion: 83, topico: "Animals" },
+        { nombre: "The Social Dilemma", año: 2020, duracion: 90, topico: "Technology" },
+      ];
+
+      const documentariesCollection = new DocumentalesCollection(documentaries);
+
+      const result = documentariesCollection.searchItemByTopic("Animals");
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Blackfish",
+        año: 2013,
+        duracion: 83,
+        topico: "Animals",
+      });
+    });
+  });
+});
+
+describe("PeliculasCollection", () => {
+  describe("searchItemByDuration", () => {
+    it("deberia devolver un array de peliculas de esa duración", () => {
+      const movies = [
+        { nombre: "Inception", año: 2010, duracion: 152 },
+        { nombre: "The Godfather", año: 1972, duracion: 175 },
+        { nombre: "The Dark Knight", año: 2008, duracion: 152 },
+      ];
+
+      const moviesCollection = new PeliculasCollection(movies);
+
+      const result = moviesCollection.searchItemByDuration(152);
+
+      expect(result).to.have.lengthOf(2);
+      expect(result[0]).to.deep.include({
+        nombre: "Inception",
+        año: 2010,
+        duracion: 152,
+      });
+      expect(result[1]).to.deep.include({
+        nombre: "The Dark Knight",
+        año: 2008,
+        duracion: 152,
+      });
+    });
   });
 });
