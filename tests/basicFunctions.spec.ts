79253631d43ec8526a4cec612a4ac4bef4577a5a
Diego Arnold Wiederkehr Bruno <alu0101601830@ull.edu.es>
2023-03-07 13:05:24 +0000

    Coverage ejercicio 1

--- tests/basicFunctions.spec.ts
+++ tests/basicFunctions.spec.ts
@@ -70,6 +70,27 @@ describe("SeriesCollection", () => {
       });
     });
   });
+
+  describe("searchItemByName", () => {
+    it("deberia devolver un array de series que coincidan con el nombre", () => {
+      const series = [
+        { nombre: "Breaking Bad", año: 2008, temporadas: 5 },
+        { nombre: "Game of Thrones", año: 2011, temporadas: 8 },
+        { nombre: "Friends", año: 1994, temporadas: 10 },
+      ];
+
+      const seriesCollection = new SeriesCollection(series);
+
+      const result = seriesCollection.searchItemByName("Friends");
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Friends",
+        año: 1994,
+        temporadas: 10,
+      });
+    });
+  });
 });
 
 describe("DocumentalesCollection", () => {
@@ -84,6 +105,50 @@ describe("DocumentalesCollection", () => {
       const documentariesCollection = new DocumentalesCollection(documentaries);
 
       const result = documentariesCollection.searchItemByTopic("Animals");
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Blackfish",
+        año: 2013,
+        duracion: 83,
+        topico: "Animals",
+      });
+    });
+  });
+
+  describe("searchItemByDuration", () => {
+    it("deberia devolver un array de documentales con esa duración", () => {
+      const documentaries = [
+        { nombre: "Planet Earth", año: 2006, duracion: 300, topico: "Nature" },
+        { nombre: "Blackfish", año: 2013, duracion: 83, topico: "Animals" },
+        { nombre: "The Social Dilemma", año: 2020, duracion: 90, topico: "Technology" },
+      ];
+
+      const documentariesCollection = new DocumentalesCollection(documentaries);
+
+      const result = documentariesCollection.searchItemByDuration(83);
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Blackfish",
+        año: 2013,
+        duracion: 83,
+        topico: "Animals",
+      });
+    });
+  });
+
+  describe("searchItemByYear", () => {
+    it("deberia devolver un array de documentales con ese año de lanzamiento", () => {
+      const documentaries = [
+        { nombre: "Planet Earth", año: 2006, duracion: 300, topico: "Nature" },
+        { nombre: "Blackfish", año: 2013, duracion: 83, topico: "Animals" },
+        { nombre: "The Social Dilemma", año: 2020, duracion: 90, topico: "Technology" },
+      ];
+
+      const documentariesCollection = new DocumentalesCollection(documentaries);
+
+      const result = documentariesCollection.searchItemByYear(2013);
 
       expect(result).to.have.lengthOf(1);
       expect(result[0]).to.deep.include({
@@ -118,6 +183,45 @@ describe("PeliculasCollection", () => {
       expect(result[1]).to.deep.include({
         nombre: "The Dark Knight",
         año: 2008,
+        duracion: 152,
+      });
+    });
+  });
+  describe("searchItemByYear", () => {
+    it("deberia devolver un array con las peliculas de ese año", () => {
+      const peliculas = [
+        { nombre: "The Dark Knight", año: 2008, duracion: 152 },
+        { nombre: "Inception", año: 2010, duracion: 152 },
+      ];
+
+      const peliculasCollection = new PeliculasCollection(peliculas);
+
+      const result = peliculasCollection.searchItemByYear(2010);
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Inception",
+        año: 2010,
+        duracion: 152,
+      });
+    });
+  });
+
+  describe("searchItemByName", () => {
+    it("deberia devolver un array con las peliculas con ese nombre", () => {
+      const peliculas = [
+        { nombre: "The Dark Knight", año: 2008, duracion: 152 },
+        { nombre: "Inception", año: 2010, duracion: 152 },
+      ];
+
+      const peliculasCollection = new PeliculasCollection(peliculas);
+
+      const result = peliculasCollection.searchItemByName("Inception");
+
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.deep.include({
+        nombre: "Inception",
+        año: 2010,
         duracion: 152,
       });
     });
